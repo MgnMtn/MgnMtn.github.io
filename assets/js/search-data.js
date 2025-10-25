9f64277365ee62d0606ffafbe6b5f509aecadac0
MgnMtn <megan.maton@gmail.com>
2025-10-25 16:25:33 +0000

    Deploying to gh-pages from @ MgnMtn/MgnMtn.github.io@968d2e3640bf84d53b8f2a83a7e1c5ebe7f5734b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,26 +4,26 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-home",
-    title: "home",
+    title: "Home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "news-enjoyed-our-time-in-flagstaff-arizona-at-icsme-2024-presenting-our-papers-exploring-pseudo-testedness-empirically-evaluating-extreme-mutation-testing-at-the-statement-level-and-pseudosweep-a-pseudo-tested-code-identifier",
-          title: 'Enjoyed our time in Flagstaff, Arizona at ICSME 2024 presenting our papers “...',
+          title: '🇺🇸 Enjoyed our time in Flagstaff, Arizona at ICSME 2024 presenting our papers...',
           description: "",
           section: "News",},{id: "news-excited-to-have-our-paper-where-tests-fall-short-empirically-analyzing-oracle-gaps-in-covered-code-accepted-at-esem-2025",
-          title: 'Excited to have our paper “Where Tests Fall Short: Empirically Analyzing Oracle Gaps...',
+          title: '✅ Excited to have our paper “Where Tests Fall Short: Empirically Analyzing Oracle...',
           description: "",
-          section: "News",},{id: "news-had-a-great-time-at-esem-2025-both-as-a-student-volunteer-and-presenting-our-paper-where-tests-fall-short-empirically-analyzing-oracle-gaps-in-covered-code",
-          title: 'Had a great time at ESEM 2025 both as a student volunteer and...',
+          section: "News",},{id: "news-had-a-great-time-at-esem-2025-in-honolulu-hawaii-as-a-student-volunteer-and-presenting-our-paper-where-tests-fall-short-empirically-analyzing-oracle-gaps-in-covered-code",
+          title: '🇺🇸 Had a great time at ESEM 2025 in Honolulu, Hawaii, as a...',
           description: "",
           section: "News",},{
         id: 'social-email',
