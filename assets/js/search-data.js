cc812578820c429ffa50da5a8fd9684915330455
MgnMtn <megan.maton@gmail.com>
2025-10-25 19:43:02 +0000

    Deploying to gh-pages from @ MgnMtn/MgnMtn.github.io@05f92e8872c7d6ed0402efc266580abc3b29440d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -22,8 +22,8 @@ ninja.data = [{
           section: "News",},{id: "news-excited-to-have-our-paper-where-tests-fall-short-empirically-analyzing-oracle-gaps-in-covered-code-accepted-at-esem-2025",
           title: '✅ Excited to have our paper “Where Tests Fall Short: Empirically Analyzing Oracle...',
           description: "",
-          section: "News",},{id: "news-had-a-great-time-at-esem-2025-in-honolulu-hawaii-as-a-student-volunteer-and-presenting-our-paper-where-tests-fall-short-empirically-analyzing-oracle-gaps-in-covered-code",
-          title: '🇺🇸 Had a great time at ESEM 2025 in Honolulu, Hawaii, as a...',
+          section: "News",},{id: "news-had-a-great-time-at-esem-2025-in-honolulu-hawaii-both-as-a-student-volunteer-and-presenting-our-paper-where-tests-fall-short-empirically-analyzing-oracle-gaps-in-covered-code",
+          title: '🇺🇸 Had a great time at ESEM 2025 in Honolulu, Hawaii, both as...',
           description: "",
           section: "News",},{
         id: 'social-github',
